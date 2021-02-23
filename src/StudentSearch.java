a706883fae62f5df74940559189f7ab4d7e1e1ad
Lara Fenercioğlu <larafenercioglu@gmail.com>
2021-02-23 16:29:25 +0300

    add comments

--- src/StudentSearch.java
+++ src/StudentSearch.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class StudentSearch {
+public class StudentSearch { //StudentSearch Class
     public boolean studentExists(ArrayList<Student> students, String id) throws Exception {
         if (students.isEmpty()) {
             throw new Exception("Students list should not be empty!");
